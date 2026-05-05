68a4e524b615f2349b9bcabd577c61c7a29fbde3
Giles Knap <giles.knap@diamond.ac.uk>
2026-05-05 11:33:08 +0100

    fix: refine helm scripts

--- scripts/dls-install-hylas.sh
+++ scripts/dls-install-hylas.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-helm upgrade --install argocd-monitor oci://ghcr.io/epics-containers/charts/argocd-monitor \
-    --version 0.5.2 \
+helm upgrade --install hylas-argocd-monitor oci://ghcr.io/epics-containers/charts/argocd-monitor \
+    --version 0.5.3-beta.1 \
     --namespace argocd-monitor \
-    --set image.tag=0.5.2 \
     --set ingress.enabled=true \
     --set ingress.host=hylas-argocd-monitor.diamond.ac.uk \
     --set argocd.url=https://argocd-controls.diamond.ac.uk \
